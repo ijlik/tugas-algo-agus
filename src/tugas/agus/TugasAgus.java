e0c9b043931809ed20b639343bcb270bec875528
s-ijlik <mail.coffejava@gmail.com>
2018-04-01 17:50:16 +0700

    jawaban terselesaikan

--- src/tugas/agus/TugasAgus.java
+++ src/tugas/agus/TugasAgus.java
@@ -5,6 +5,8 @@
  */
 package tugas.agus;
 
+import java.util.Scanner;
+
 /**
  *
  * @author r-ijlik
@@ -15,7 +17,39 @@ public class TugasAgus {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        String hari;
+        int dewasa, anak, tiketDewasa, tiketAnak, diskon = 0, total, diskonAnak = 0;
+        
+        System.out.print("Masukan Hari : ");
+        hari = sc.next();
+        System.out.print("Dewasa : ");
+        dewasa = sc.nextInt();
+        System.out.print("Anak - anak : ");
+        anak = sc.nextInt();
+        
+        switch(hari){
+            case "minggu":
+                tiketDewasa = 35000;
+                tiketAnak = 20000;
+                if ((dewasa + anak) > 5) {
+                    diskon = 10000;
+                }
+                if ((dewasa + anak) > 10 && anak != 0){
+                    diskonAnak = 5000;
+                }
+                total = (dewasa * tiketDewasa) + (anak * (tiketAnak - diskonAnak)) - diskon;
+                break;
+            default:
+                tiketDewasa = 25000;
+                tiketAnak = 15000;
+                if ((dewasa + anak) > 10) {
+                    diskon = 5000;
+                }
+                total = (dewasa * (tiketDewasa - diskon)) + ((anak * (tiketAnak - diskon)));
+                break;
+        }
+        System.out.println("Sekarang hari "+hari+"\nJumlah Dewasa : "+dewasa+"\nJumlah Anak-anak : "+anak+"\n\nTotal harga = "+total);
     }
     
 }
